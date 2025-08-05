7bf6b3c1c230c886639ef6c7286b4d9435d17791
TenzinTsogney <tsogney99@gmail.com>
2025-08-05 16:15:57 +0530

    commit

--- Backend/utils/api.js
+++ Backend/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://melodious-sawine-21ea88.netlify.app/api", // change if deployed
+  baseURL: "dapper-croissant-695190.netlify.app/api", // change if deployed
 });
 
 export default API;
